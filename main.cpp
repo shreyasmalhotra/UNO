0dbc0cd2340191682842af570025b01e0ec18f23
NaseerL <82166449+NaseerL@users.noreply.github.com>
2021-05-12 18:34:06 -0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -7,82 +7,55 @@
 
 using namespace std;
 
-/**
- * Function to clear the terminal by inserting many new line characters.
- */
-void clearTerminal();
+void clearTerminal(); // Function to clear the terminal by inserting many new line characters.
 
-/**
- * Function to populate the deck with cards.
- * 
- * @param deck A vector reference of Card pointers representing the deck
- */
-void buildDeck(vector<Card*>& deck);
+void buildDeck(vector<Card*>& deck);/* Function to populate the deck with cards.
+                                       @param deck A vector reference of Card pointers representing the deck */
 
-/**
- * Function to shuffle the deck.
- * 
- * @param deck A vector reference of Card pointers representing the deck
- */
- void shuffleDeck(vector<Card*>&);
+ void shuffleDeck(vector<Card*>&);/* Function to shuffle the deck.
+                                     @param deck A vector reference of Card pointers representing the deck */
 
-/**
- * Function to draw a card from the deck to either the hand or discard pile.
- * 
- * @param deck A vector reference of Card pointers representing the deck
- * @param target A vector representing the structure that the card being drawn 
- * from the deck is being drawn to. This can be either a hand or the discard
- * pile
- */
-void drawCards(vector<Card*>& deck, vector<Card*>& target, int);
 
-/**
- * Function to draw 7 cards to each player's hand at the beginning of the game.
- * 
- * @param deck A vector reference of Card pointers representing the deck.
- * @param hands A vector of vector of Card pointers representing each player 
- * and their hands. The indicies of the first vector represents a player, and 
- * the indicies of the vector at that index is each individual card within the
- * player's hand.
- */
-void populateHands(vector<Card*>& deck, vector<vector<Card*>>& hands);
 
-/** 
- * Renders the cards in he hand vector passed.
- * 
- * @param hand A vector containing Card pointers
- */
-void renderHand(vector<Card*> hand);
+void drawCards(vector<Card*>& deck, vector<Card*>& target, int);/* Function to draw a card from the deck to either the hand or discard pile.
+                                                                  
+                                                                   @param deck A vector reference of Card pointers representing the deck
+                                                                   @param target A vector representing the structure that the card being drawn 
+                                                                   from the deck is being drawn to. This can be either a hand or the discard pile */
 
-/**
- * Renders the top card of the passed discard vector.
- * 
- * @param hand A vector containing Card pointers
- */
-void renderDiscard(vector<Card*>);
+void populateHands(vector<Card*>& deck, vector<vector<Card*>>& hands);/* Function to draw 7 cards to each player's hand at the beginning of the game.
+                                                                     
+                                                                         @param deck A vector reference of Card pointers representing the deck.
+                                                                         @param hands A vector of vector of Card pointers representing each player 
+                                                                         and their hands. The indicies of the first vector represents a player, and 
+                                                                         the indicies of the vector at that index is each individual card within the 
+                                                                         player's hand. */
 
-/**
- * Passed references to the deck, hand, and discard vectors and a reference to 
- * the game state. This function will resolve the turn for the current player
- * (whose index is stored in the game state). TakeTurn will query the user for
- * input, draw cards if the previous card was a draw 2 card, or skip the current
- * players turn (see numCardsToPlay in GameState).
- * 
- * @param deck A vector reference of Card pointers representing the deck
- * @param hand A vector reference of Card pointers representing a single player's hand
- * @param discard A vector reference of Card pointers representing the discard pile
- * @param gameState A reference of the game state object
- */
-void takeTurn(vector<Card*>& deck, vector<Card*>& hand, vector<Card*>& discard, GameState& gameState);
+void renderHand(vector<Card*> hand);/* Renders the cards in he hand vector passed.
+                                       
+                                       @param hand A vector containing Card pointers */
+
+void renderDiscard(vector<Card*>);/* Renders the top card of the passed discard vector.
+                                     
+                                     @param hand A vector containing Card pointers */
+
+bool takeTurn(vector<Card*>& deck, vector<Card*>& hand, vector<Card*>& discard, GameState& gameState);
+/* Passed references to the deck, hand, and discard vectors and a reference to 
+   the game state. This function will resolve the turn for the current player
+   (whose index is stored in the game state). TakeTurn will query the user for
+   input, draw cards if the previous card was a draw 2 card, or skip the current
+   players turn (see numCardsToPlay in GameState).
+   
+   @param deck A vector reference of Card pointers representing the deck
+   @param hand A vector reference of Card pointers representing a single player's hand
+   @param discard A vector reference of Card pointers representing the discard pile
+   @param gameState A reference of the game state object */
 
 int main(){
     srand(time(0));
-    int NUM_PLAYERS;
+    const int NUM_PLAYERS = 2;
     bool doneWithUNO = false;
-    cout << "enter the number of player's :";
-    cin >> NUM_PLAYERS;
     GameState gameState(NUM_PLAYERS);
-    
     
     vector<Card*> deck;
     vector<Card*> discard;
@@ -94,9 +67,12 @@ int main(){
     populateHands(deck, hands);
     drawCards(deck, discard, 1);
     
-    while(doneWithUNO != true){
-        takeTurn(deck, hands.at(gameState.currentPlayerIndex), discard, gameState);
-    }
+    while(doneWithUNO != true) {
+        doneWithUNO = takeTurn(deck, hands.at(gameState.currentPlayerIndex), discard, gameState);
+        }
+    
+    
+    
     
     return 0;
 }
@@ -153,8 +129,6 @@ void buildDeck(vector<Card*> &deck){
         }
     }
 }
-    
-
 
 void shuffleDeck(vector<Card*> &deck){
     for(int i = 0; i < 1000; i++){
@@ -203,11 +177,13 @@ void renderDiscard(vector<Card*> discard){
     }
 }
 
-void takeTurn(vector<Card*> &deck, vector<Card*> &hand, vector<Card*> &discard, GameState &gameState){
-    clearTerminal();
+bool takeTurn(vector<Card*> &deck, vector<Card*> &hand, vector<Card*> &discard, GameState &gameState){
+    bool noCardsLeft = false;
+    
+    //clearTerminal();
     renderDiscard(discard);
     cout << "Player " << gameState.currentPlayerIndex << "'s turn." << endl;
-    //cout << gameState.retTurnDirection();
+    
     // TODO: Draw cards if necessary (draw 2 card)
     drawCards(deck, hand, gameState.numCardsToDraw);
     gameState.numCardsToDraw = 0; // reset cards to draw back to 0
@@ -215,12 +191,12 @@ void takeTurn(vector<Card*> &deck, vector<Card*> &hand, vector<Card*> &discard, 
     renderHand(hand);
     
     // loop for number of cards to play (0 if previously played card was a "skip" or "draw 2")
-    
+    //for(int j = 0; j < gameState.numCardsToPlay; j++){
     if(!gameState.skipTurn){
         // Collect user input
         cout << "What would you like to do?" << endl;
         int i;
-        for(i = 0; i < hand.size(); i++){
+        for(i = 0; i < hand.size(); i++){// Prints out all options
             cout << i << ": Play Card" << endl;
         }
         cout << i << ": Draw a Card" << endl;
@@ -229,28 +205,36 @@ void takeTurn(vector<Card*> &deck, vector<Card*> &hand, vector<Card*> &discard, 
         
         
         // Evaluate user input
-        if(input < i){
-            
-            
-            
+        if (input < i) {
             // Play card at index input
             if(hand.at(input)->play(discard.at(discard.size()-1), gameState)){
                 Card* temp;
                 temp = hand.at(input);
                 discard.push_back(temp);
                 hand.erase(hand.begin() + input); // Remove card in hand at position "input"
-            }else {
+                if (hand.size() == 0) {
+                    noCardsLeft = true;
+                }
+                else if (hand.size() == 1) {
+                    cout << "UNO!!!" << endl;
+                }
+            } 
+            else {
                 cout << "Improper choice" << endl;
                 takeTurn(deck, hand, discard, gameState);
-                return;
+                return false;
             }
-        }else if(input == i){
+        }
+        else if (input == i) {
             drawCards(deck, hand, 1);
         }
-    }else{
+    }
+    else {
         gameState.skipTurn = false;
     }
     
     // update variables for next turn
     gameState.nextTurn();
+    
+    return noCardsLeft;
 }
